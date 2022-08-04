d04f29d96a048425d937cbb08f74a21c45175813
zui88 <marco.burkhardt@hotmail.de>
2022-08-05 00:47:54 +0200

    Curiously Recurring Template Pattern for counter classes

--- src/zui/util/counter.h
+++ src/zui/util/counter.h
@@ -8,11 +8,9 @@ namespace zui {
   
   typedef std::size_t counter_type;
 
-
-  /**
-   * A small class for counting references
-   */
-  struct counter {
+  
+  template<typename T>
+  struct counter_functions {
     static constexpr const counter_type MIN = std::numeric_limits<counter_type>::min();
     static constexpr const counter_type MAX = std::numeric_limits<counter_type>::max();
 
@@ -20,39 +18,23 @@ namespace zui {
       empty = MIN,
     };
     
-    counter () = default;
-    ~counter () = default;
-    counter (counter_type cnt)
-      : m_cnt{ cnt } { }
-    /**
-     * Reset the couter
-     */
-    counter(const counter &cnt) : m_cnt{ MIN } {}
-
     void                       cnt_inc()                       noexcept { this->operator++(); }
     void                       cnt_dec()                       noexcept { this->operator--(); }
-    void                       cnt_reset()                     noexcept { m_cnt = MIN; }
-    void                       cnt_set(counter_type cnt)       noexcept { m_cnt = cnt; }
-    [[nodiscard]] counter_type cnt_get()                 const noexcept { return m_cnt; }
+    void                       cnt_reset()                     noexcept { mh_underlying->cnt_set( MIN ); }
 
-    [[nodiscard]] counter_type operator()() const noexcept { return m_cnt; }
+    [[nodiscard]] counter_type operator()() const noexcept { return mh_underlying->cnt_get(); }
     // just preinkrement
     counter_type operator++() noexcept {
-      if (m_cnt < MAX) 
-	m_cnt++;
-      return m_cnt;
+      auto tmpCnt = mh_underlying->cnt_get();
+      if (tmpCnt < MAX) 
+	return mh_underlying->cnt_set(tmpCnt + 1);
+      return tmpCnt;
     }
     counter_type operator--() noexcept {
-      if (m_cnt != MIN)
-	m_cnt--;
-      return m_cnt;
-    }
-    /**
-     * Reset the couter
-     */
-    counter &operator=(counter_type& cnt) noexcept {
-      cnt = MIN;
-      return *this;
+      auto tmpCnt = mh_underlying->cnt_get();
+      if (tmpCnt != MIN)
+	return mh_underlying->cnt_set(tmpCnt - 1);
+      return tmpCnt;
     }
 
     [[nodiscard]] static inline counter_type cnt_min() noexcept {
@@ -63,29 +45,34 @@ namespace zui {
     }
 
   private:
+    T* mh_underlying = static_cast<T*>(this);
+  };
+
+#define CNT_SET(XCNT)                                                          \
+  counter_type cnt_set(counter_type cnt) noexcept { XCNT = cnt; }
+#define CNT_GET(XCNT)                                                          \
+  [[nodiscard]] counter_type cnt_get() const noexcept { return XCNT; }
+
+  /**
+   * A small class for counting references
+   */
+  struct counter : counter_functions<counter>{
+    CNT_SET(m_cnt)
+    CNT_GET(m_cnt)
+  private:
     counter_type m_cnt { MIN };
   };
 
 
+  /**
+   * A small class for counting static references
+   */
   template<typename Derived>
-  struct static_counter {
-    counter_type cnt_inc() noexcept {
-      ++s_count;
-      return cnt_get();
-    }
-
-    counter_type cnt_dec() noexcept {
-      --s_count;
-      return cnt_get();
-    }
-
-    counter_type operator++() noexcept { return cnt_inc(); }
-    counter_type operator--() noexcept { return cnt_dec(); }
-
-    counter_type cnt_get() const noexcept { return s_count.cnt_get(); }
-
+  struct static_counter : counter_functions<static_counter<Derived>>{
+    CNT_SET(s_cnt)
+    CNT_GET(s_cnt)
   private:
-    static inline counter s_count;
+    static inline counter_type s_cnt {};
   };
 
 
